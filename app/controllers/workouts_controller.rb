753b64305b1106030dc985256bd2b15a68562996
Kai Powell <kai@nationbuilder.com>
2017-02-26 13:49:23 -0800

    Get index view working

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 # Handles requests around workouts
 class WorkoutsController < ApplicationController
+  def index
+    @workouts = Workout.where(user_id: current_user.id).all
+  end
+
   def new
     @workout = Workout.new
     2.times { @workout.cardio_exercises.build }
@@ -8,5 +12,15 @@ class WorkoutsController < ApplicationController
       exercise = @workout.strength_exercises.build
       5.times { exercise.sets.build }
     end
+  end
+
+  def create
+    @workout = Workout.new(params.require(:workout)
+                           .permit(:date,
+                                   cardio_exercises: [:exercise, :minutes],
+                                   strength_exercises: { exercise: [], sets: [:weight, :reps] }))
+    @workout.user = current_user
+    @workout.save!
+    redirect_to :root
   end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,4 +3,8 @@ class Workout < ApplicationRecord
   belongs_to :user, inverse_of: :workouts
   has_many :cardio_exercises, inverse_of: :workout
   has_many :strength_exercises, inverse_of: :workout
+
+  validates_presence_of :date
+
+  accepts_nested_attributes_for :cardio_exercises
 end
